b03a948412fdcd0b9a0aa8c75e93cbbf80983db5
沛 周 <1490573790@qq.com>
2024-07-01 16:33:23 +0800

    添加项目文件。

---
+++ dataReadingTest01.cpp
@@ -0,0 +1,152 @@
+﻿#include <iostream>
+#include<string>
+#include<vector>
+#include<map>
+#include<list>
+#include<dirent.h>
+#include"satobs.h"
+
+using namespace std;
+
+void getFilesInFolder(const char* folderPath)
+{
+	//读文件夹
+	DIR* dir;
+	struct dirent* ent;
+	if ((dir = opendir(folderPath)) == NULL)
+	{
+		printf("Cna't open folder %s\n！", folderPath);
+		return;
+	}
+	while ((ent = readdir(dir)) != NULL) {//读取目录中的文件
+		if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)continue;//跳过"."和".."
+		printf("%s%s\n", folderPath, ent->d_name);//输出文件名
+	}
+	closedir(dir);//关闭目录
+
+}
+
+
+int main()
+{
+	satobs data;
+	vector<satobs> dataVector;
+	map<UTC, satobs> dataMap;
+	list<satobs> dataList;
+	vector<map<UTC, satobs>> dataAll;
+	map<UTC, vector<satobs>> dataMapAll;
+	//auto it = dataMapAll.begin();
+	//char FileName[256] = "data\\obsdat\\r_009000101_005001001.ttt";
+	//
+	//char* saveptr; // 用于保存 strtok_r 的内部状态
+	//FILE* obsFile;
+	//char buffer[256];
+	//
+	//if (fopen_s(&obsFile, FileName, "r+b"))
+	//{
+	//	printf("The observation data file %s was not opened!\n", FileName);
+	//	return -1;
+	//}	
+	//fgets(buffer, 256, obsFile);
+	//while (!feof(obsFile))
+	//{
+	//	//char* token;
+	//	data.obsType = strtok_s(buffer, " ", &saveptr);//测量类型
+	//	data.stationID = strtok_s(NULL, " ", &saveptr);//测站ID
+	//	data.satlliteID = strtok_s(NULL, " ", &saveptr);//卫星ID
+	//	data.obsSign = strtok_s(NULL, " ", &saveptr);//数据标识
+	//	data.time.year = atoi(strtok_s(NULL, " ", &saveptr));//UTC 年
+	//	data.time.day = atoi(strtok_s(NULL, " ", &saveptr));//UTC 年积日
+	//	data.time.sod = atof(strtok_s(NULL, " ", &saveptr));//UTC 秒
+	//	data.distance = atoi(strtok_s(NULL, " ", &saveptr))+ atof(strtok_s(NULL, " ", &saveptr))*1e-9;//UTC 年积日
+
+	//	dataVector.push_back(data);
+	//	dataList.push_back(data);
+	//	dataMap[data.time] = data;
+	//	fgets(buffer, 256, obsFile);
+	//}
+
+
+	const char* folderPath = "data\\obsdat\\";
+
+	//读文件夹
+	DIR* dir;
+	struct dirent* ent;
+	if ((dir = opendir(folderPath)) == NULL)
+	{
+		printf("Cna't open folder %s\n！", folderPath);
+		return -5;//文件夹读取失败
+	}
+	while ((ent = readdir(dir)) != NULL)//读取目录中的文件
+	{
+		char FilePath[256]="";
+		if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)continue;//跳过"."和".."
+		//printf("%s%s\n", folderPath, ent->d_name);//输出文件名
+		strcat_s(FilePath,256,folderPath);
+		strcat_s(FilePath, 256, ent->d_name);
+		//printf("%s\n", FilePath);//输出文件名
+
+		//读取每个文件
+		char* saveptr=NULL; // 用于保存 strtok_r 的内部状态
+		FILE* obsFile;
+		char buffer[256];
+
+		if (fopen_s(&obsFile, FilePath, "r+b"))
+		{
+			printf("The observation data file %s was not opened!\n", FilePath);
+			continue;
+			//return -1;
+		}
+		fgets(buffer, 256, obsFile);
+		while (!feof(obsFile))
+		{
+			//saveptr = NULL;
+			//char* token;
+			//data = satobs();
+		
+			strcpy_s(data.obsType, strtok_s(buffer, " ", &saveptr));//测量类型
+			strcpy_s(data.stationID, strtok_s(NULL, " ", &saveptr));//测站ID
+			strcpy_s(data.satlliteID, strtok_s(NULL, " ", &saveptr));//卫星ID
+			strcpy_s(data.obsSign, strtok_s(NULL, " ", &saveptr));//数据标识
+			//data.obsType = strtok_s(buffer, " ", &saveptr);//测量类型
+			//data.stationID = strtok_s(NULL, " ", &saveptr);//测站ID
+			//data.satlliteID = strtok_s(NULL, " ", &saveptr);//卫星ID
+			//data.obsSign = strtok_s(NULL, " ", &saveptr);//数据标识
+			data.time.year = atoi(strtok_s(NULL, " ", &saveptr));//UTC 年
+			data.time.day = atoi(strtok_s(NULL, " ", &saveptr));//UTC 年积日
+			data.time.sod = atof(strtok_s(NULL, " ", &saveptr));//UTC 秒
+			data.distance = atoi(strtok_s(NULL, " ", &saveptr)) + atof(strtok_s(NULL, " ", &saveptr)) * 1e-9;//UTC 年积日
+
+			//dataVector.push_back(data);
+			//dataList.push_back(data);
+			//dataMap[data.time] = data;
+			dataMapAll[data.time].push_back(data);
+			memset(buffer, '\0', sizeof(buffer));
+			fgets(buffer, 256, obsFile);
+		}
+		
+		//dataAll.push_back(dataMap);
+		fclose(obsFile);
+		
+	}
+	closedir(dir);//关闭目录
+	auto it = dataMapAll.begin();
+	for (; it != dataMapAll.end(); it++)
+	{
+		int num = 0;
+		for (int i = 0; i < it->second.size(); i++)
+		{
+			for (int j = 0; j < it->second.size(); j++)
+			{
+				if (strcmp(it->second[i].stationID, it->second[j].satlliteID) == 0 && strcmp(it->second[j].stationID, it->second[i].satlliteID) == 0)
+				{
+					num++;
+				}
+			}
+		}
+
+	}
+
+	return 0;
+}
+
---
+++ satobs.h
@@ -0,0 +1,117 @@
+#pragma once
+#ifndef satobs_H
+#define satobs_H
+
+struct GPST
+{
+	short week;
+	double sow;//second of GPS week
+	GPST()
+	{
+		week = 0;
+		sow = 0.0;
+	}
+
+};
+
+struct UTC
+{
+	short year;
+	short day;//day of year
+	double sod;//second of day
+	UTC()
+	{
+		year = 0;
+		day = 0;
+		sod = 0.0;
+	}
+};
+
+struct satobs
+{
+	char obsType[6]; //02101标识地基测距，02401标识星间测距
+	char stationID[10] ;
+	char satlliteID[10];
+	char obsSign[8];
+	UTC time;
+	double distance;//单位：KM，D向下取整为第一个整数，将D的小数部分乘1e9（10^9）取整为第二个整数
+	satobs()
+	{
+		memset(obsType, '\0', sizeof(obsType));
+		memset(stationID, '\0', sizeof(stationID));
+		memset(satlliteID, '\0', sizeof(satlliteID));
+		memset(obsSign, '\0', sizeof(obsSign));
+		distance = 0.0;
+	}
+};
+
+//时间比较
+inline bool& operator <(const GPST& gt1, const GPST& gt2)
+{
+	bool ok = false;
+	if ((gt1.week * 604800 + gt1.sow) < (gt2.week * 604800 + gt2.sow))
+		ok = true;
+	else ok = false;
+	return ok;
+}
+
+inline bool& operator >(const GPST& gt1, const GPST& gt2)
+{
+	bool ok = false;
+	if ((gt1.week * 604800 + gt1.sow)  > (gt2.week * 604800 + gt2.sow))
+		ok = true;
+	else ok = false;
+	return ok;
+}
+
+inline bool& operator ==(const GPST& gt1, const GPST& gt2)
+{
+	bool ok = false;
+	if ((gt1.week * 604800 + gt1.sow) == (gt2.week * 604800 + gt2.sow))
+		ok = true;
+	else ok = false;
+	return ok;
+}
+
+//UTC
+inline bool& operator <(const UTC& t1, const UTC& t2)
+{
+	bool ok = false;
+	if (t1.year < t2.year) ok = true;
+	else if(t1.year == t2.year)
+	{
+		if (t1.day * 86400 + t1.sod < t2.day * 86400 + t2.sod)
+			ok = true;
+		else ok = false;
+	}
+	else ok = false;
+	return ok;
+}
+
+inline bool& operator  > (const UTC& t1, const UTC& t2)
+{
+	bool ok = false;
+	if (t1.year > t2.year) ok = true;
+	else if (t1.year == t2.year)
+	{
+		if (t1.day * 86400 + t1.sod > t2.day * 86400 + t2.sod)
+			ok = true;
+		else ok = false;
+	}
+	else ok = false;
+	return ok;
+}
+
+inline bool& operator  == (const UTC& t1, const UTC& t2)
+{
+	bool ok = false;
+	if (t1.year == t2.year)
+	{
+		if (t1.day * 86400 + t1.sod == t2.day * 86400 + t2.sod)
+			ok = true;
+		else ok = false;
+	}
+	else ok = false;
+	return ok;
+}
+#endif // !satobs_H
